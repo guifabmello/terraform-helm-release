6977b3ef22a5fe5f5355f84d893ff2c8c63ca247
Edmilson da Silva Alferes <edmilson.alferes@ibopemedia.kantar.com>
2020-04-06 10:05:26 -0300

    add test

--- test/release_test.go
+++ test/release_test.go
@@ -6,17 +6,11 @@ import (
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
-func TestTerraformAzureAKS(t *testing.T) {
+func TestTerraformHelmRelease(t *testing.T) {
 
 	terraformOptions := &terraform.Options{
 		// The path to where your Terraform code is located
 		TerraformDir: "../examples",
-
-		// Variables to pass to our Terraform code using -var options
-		//Vars: map[string]interface{}{
-		//	"service_principal_id":     os.Getenv("ARM_CLIENT_ID"),
-		//	"service_principal_secret": os.Getenv("ARM_CLIENT_SECRET"),
-		//},
 	}
 
 	// At the end of the test, run `terraform destroy` to clean up any resources that were created
@@ -24,5 +18,4 @@ func TestTerraformAzureAKS(t *testing.T) {
 
 	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
 	terraform.InitAndApply(t, terraformOptions)
-
 }
