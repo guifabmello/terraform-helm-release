018a241244690283e634afceac524de04eb0dee1
Edmilson da Silva Alferes <edmilson.alferes@ibopemedia.kantar.com>
2020-05-18 19:53:12 -0300

    The namespace will only be created if it does not exist. An efficient workaround to solve helm3's deficiency does not create namespaces.

--- resources.tf
+++ resources.tf
@@ -1,14 +1,11 @@
-resource "kubernetes_namespace" "this" {
+# The namespace will only be created if it does not exist. 
+# An efficient workaround to solve helm3  deficiency does not create namespaces.
+resource "null_resource" "create_namespace" {
+
   for_each = var.release
-  metadata {
-    annotations = {
-      name = substr(each.key, 0, 100)
-    }
-
-    labels = {
-      provisioner = "terraform"
-    }
-
-    name = substr(each.key, 0, 100)
+  
+  provisioner "local-exec" {
+    
+    command = "kubectl get namespace ${substr(each.key, 0, 100)} >/dev/null 2>&1 || kubectl create namespace ${substr(each.key, 0, 100)}"
   }
 }
